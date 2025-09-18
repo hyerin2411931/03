19b0130d5551c3945a515b8191d37d4b7da19911
hyerin2411931 <2411931@sookmyung.ac.kr>
2025-09-18 16:41:19 +0900

    Week03_03

--- main.c
+++ main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    char c;
-    int i;
-    
-    printf("input a number: ");
+    char c,d ;
+    printf("enter a character : ");
     scanf("%c", &c);
     
-    i = c - '0';
-    printf("The input number is %i\n", i);
+    d = c + 1;
+    printf("The character of %c (%i) is %c (%i).\n", c, c, d, d);
+    
   system("PAUSE");	
   return 0;
 }
+
